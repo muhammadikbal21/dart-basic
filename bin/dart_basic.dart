b02064b322b8f5de014ea90eb4898e4af88316a2
muhammadikbal21 <muhammadikbal21@gmail.com>
2021-08-03 06:50:12 +0700

    function biasa dengan 2 parameter

--- bin/dart_basic.dart
+++ bin/dart_basic.dart
@@ -1,15 +1,22 @@
 import 'dart:io';
 
+double luas_segiempat(double panjang, double lebar) {
+  double hasil;
+  hasil = panjang * lebar;
+  return hasil;
+}
+
 void main(List<String> arguments) {
   
-  int a, b;
-  a = 10;
-  b = a++; // nilai b adalah 10, karena unary operator berada dibelakang variabel a, jadi variabel b hanya mengambil nilai a tanpa ++
-  print(a.toString() + " - " + b.toString());
-  
-  int c, d;
-  c = 10;
-  d = ++c; // nilai c adalah 11, karena unary operator berada didepan variabel c, jadi variabel d juga berubah nilainya seperti variabel c
-  print(c.toString() + " - " + d.toString());
+  double p, l, luas;
+
+  print('Masukkan panjang: ');
+  p = double.tryParse(stdin.readLineSync());
+  print('Masukkan lebar: ');
+  l = double.tryParse(stdin.readLineSync());
+
+  luas = luas_segiempat(p, l);
+
+  print('hasil = ' + luas.toString());
 
 }
