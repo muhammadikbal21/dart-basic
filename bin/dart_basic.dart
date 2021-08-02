791f7fe812582bc6261007a9320705137c39d3d2
muhammadikbal21 <muhammadikbal21@gmail.com>
2021-08-02 18:17:02 +0700

    parsing tipe data

--- bin/dart_basic.dart
+++ bin/dart_basic.dart
@@ -1,9 +1,11 @@
 import 'dart:io';
 
 void main(List<String> arguments) {
-  print('Input Nama anda: ');
+  print('Input angka: ');
 
   String input = stdin.readLineSync();
+  // int number = int.tryParse(input); // parsing variable tipe string menjadi integer
+  double number = double.tryParse(input); // parsing variable tipe string menjadi double
 
-  print('Nama anda: ${input}');
+  print(number + 10);
 }
