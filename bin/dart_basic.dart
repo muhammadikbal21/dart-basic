9df3b683b5e864cc90d773ba66ae4df700a9614d
muhammadikbal21 <muhammadikbal21@gmail.com>
2021-08-02 23:03:59 +0700

    perbedaan penggunaan unary increment didepan variabel maupun dibelakang variabel

--- bin/dart_basic.dart
+++ bin/dart_basic.dart
@@ -2,17 +2,14 @@ import 'dart:io';
 
 void main(List<String> arguments) {
   
-  int k = 0;
-  while (k < 5) {
-    print('unary increment ' + k.toString());
-    k++;
-  }
-
-  print('====================');
-
-  while (k > 0) {
-    print('unary decrement ' + k.toString());
-    k--;
-  }
+  int a, b;
+  a = 10;
+  b = a++; // nilai b adalah 10, karena unary operator berada dibelakang variabel a, jadi variabel b hanya mengambil nilai a tanpa ++
+  print(a.toString() + " - " + b.toString());
+  
+  int c, d;
+  c = 10;
+  d = ++c; // nilai c adalah 11, karena unary operator berada didepan variabel c, jadi variabel d juga berubah nilainya seperti variabel c
+  print(c.toString() + " - " + d.toString());
 
 }
