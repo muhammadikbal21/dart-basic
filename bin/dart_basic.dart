a25bbf8e68b374d60a0c0d78ed4d77b265db21a5
muhammadikbal21 <muhammadikbal21@gmail.com>
2021-08-03 06:53:00 +0700

    function void biasa tanpa parameter

--- bin/dart_basic.dart
+++ bin/dart_basic.dart
@@ -1,22 +1,11 @@
 import 'dart:io';
 
-double luas_segiempat(double panjang, double lebar) {
-  double hasil;
-  hasil = panjang * lebar;
-  return hasil;
+void sapa_penonton() {
+  print('hello penonton!');
 }
 
 void main(List<String> arguments) {
   
-  double p, l, luas;
-
-  print('Masukkan panjang: ');
-  p = double.tryParse(stdin.readLineSync());
-  print('Masukkan lebar: ');
-  l = double.tryParse(stdin.readLineSync());
-
-  luas = luas_segiempat(p, l);
-
-  print('hasil = ' + luas.toString());
+  sapa_penonton();
 
 }
