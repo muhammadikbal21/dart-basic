008171ca019273ad239c36fcae82d98b7e357fee
muhammadikbal21 <muhammadikbal21@gmail.com>
2021-08-02 22:16:56 +0700

    cara 5 looping counter secara menurun dikurang 2 menggunakan unary operator

--- bin/dart_basic.dart
+++ bin/dart_basic.dart
@@ -2,8 +2,8 @@ import 'dart:io';
 
 void main(List<String> arguments) {
 
-  print('cara 4');
-  for (int counter = 5; counter >= 1; counter = counter - 2) {
+  print('cara 5');
+  for (int counter = 9; counter >= 1; counter -= 2) {
     print('halo ' + counter.toString()); // jika variable bertipe integer, harus menggunakan toString untuk meng-print nya
   }
 
