36e9699890c8808e371d387da34ebe29e069383e
muhammadikbal21 <muhammadikbal21@gmail.com>
2021-08-02 18:08:09 +0700

    menggunakan input stdin dari dart

--- bin/dart_basic.dart
+++ bin/dart_basic.dart
@@ -1,3 +1,9 @@
+import 'dart:io';
+
 void main(List<String> arguments) {
-  print('Hello world!');
+  print('Input Nama anda: ');
+
+  String input = stdin.readLineSync();
+
+  print('Nama anda: ${input}');
 }
