73dd77d29b099930d4254e5d154c7660987bafa9
muhammadikbal21 <muhammadikbal21@gmail.com>
2021-08-02 22:01:47 +0700

    cara 2 menggunakan toString ketika variable tipe integer di print

--- bin/dart_basic.dart
+++ bin/dart_basic.dart
@@ -2,9 +2,9 @@ import 'dart:io';
 
 void main(List<String> arguments) {
 
-  print('cara 1');
-  for (int counter = 0; counter < 5; counter = counter + 1) {
-    print('halo');
-  }  
+  print('cara 2');
+  for (int counter = 1; counter <= 5; counter = counter + 1) {
+    print('halo ' + counter.toString()); // jika variable bertipe integer, harus menggunakan toString untuk meng-print nya
+  }
 
 }
