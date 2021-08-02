758286dc1076dd3295a1cd004bc07400a9f22e5e
muhammadikbal21 <muhammadikbal21@gmail.com>
2021-08-02 22:09:43 +0700

    cara 3 looping counter secara menurun

--- bin/dart_basic.dart
+++ bin/dart_basic.dart
@@ -2,8 +2,8 @@ import 'dart:io';
 
 void main(List<String> arguments) {
 
-  print('cara 2');
-  for (int counter = 1; counter <= 5; counter = counter + 1) {
+  print('cara 3');
+  for (int counter = 5; counter >= 1; counter = counter - 1) {
     print('halo ' + counter.toString()); // jika variable bertipe integer, harus menggunakan toString untuk meng-print nya
   }
 
