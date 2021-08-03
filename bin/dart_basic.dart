88eb603a10266d1419e21e0698964863f758cd15
muhammadikbal21 <muhammadikbal21@gmail.com>
2021-08-03 12:07:37 +0700

    penggunaan Map untuk menggabungkan string dan integer dalam List

--- bin/dart_basic.dart
+++ bin/dart_basic.dart
@@ -2,15 +2,13 @@ import 'dart:io';
 
 void main(List<String> arguments) {
   
-  List<int> myList = [];
+  List<String> myList = [];
   List<int> list = [21, 11, 2, 0, 11, 45, 30, 0, 40, 10];
 
-  // set adalah kumpulan data yang berisi elemen yang unik (tidak ada yang sama)
-  Set<int> setList;
-  setList = list.toSet();
-
-  setList.forEach((bilangan) {
-    print(bilangan);
+  myList = list.map((e) => "angka " + e.toString()).toList();
+  
+  myList.forEach((str) {
+    print(str);
   });
 
 }
