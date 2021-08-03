350d9f06914867903db4d93d73c15d87d9a7d2d1
muhammadikbal21 <muhammadikbal21@gmail.com>
2021-08-03 11:22:50 +0700

    penggunaan sublist hanya dengan indeks awal tanpa indeks akhir

--- bin/dart_basic.dart
+++ bin/dart_basic.dart
@@ -5,15 +5,10 @@ void main(List<String> arguments) {
   List<int> myList = [];
   List<int> list = [0, 1, 2, 3, 20, 30, 40, 50];
 
-  myList.addAll(list);
-  // removeWhere adalah menghapus dengan kondisi, jika kondisi terpenuhi maka elemen akan dihapus
-  // disini kita menggunakan logic untuk mendapatkan nilai ganjil, dan akan dihapus jika elemen tersebut bernilai ganjil
-  myList.removeWhere((number) => number % 2 != 0);
-
-  // contains adalah untuk memastikan bahwa List berisi elemen yang diinginkan
-  if (myList.contains(50)) {
-    print("ada!");
-  }
+  // sublist adalah mengambil sebagian dari list lain menggunakan jarak indeks awal dan indeks akhir
+  // sublist bersifat eksklusif, artinya jarak indeks terakhir elemennya tidak termasuk
+  // disini indeks terakhir tidak diberikan, maka elemen akan diambil dari yang diinginkan sampai elemen terakhir
+  myList = list.sublist(4);
 
   myList.forEach((bilangan) {
     print(bilangan);
