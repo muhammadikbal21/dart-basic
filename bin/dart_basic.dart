6e675f9b231b43416942d642760785ddcdbeca48
muhammadikbal21 <muhammadikbal21@gmail.com>
2021-08-03 07:23:08 +0700

    optional parameter dengan menggunakan named parameter

--- bin/dart_basic.dart
+++ bin/dart_basic.dart
@@ -1,20 +1,18 @@
 import 'dart:io';
 
-double luas_segiempat(double panjang, double lebar) {
-  double hasil;
-  hasil = panjang * lebar;
-  return hasil;
+// optional parameter adalah parameter yang bersifat opsional atau tambahan, jadi jika parameter ini tidak diberi argument maka tidak masalah
+// optional parameter terdiri dari 2 jenis, yaitu:
+// 1. named parameter, menggunakan tanda kurung kurawal {}
+// 2. position parameter, menggunakan tanda kurung siku []
+
+// ini contoh penggunaan named parameter
+String say(String from, String message, {String to, String appName}) {
+  return from + " say " + message + ((to != null) ? " to " + to : "") + ((appName != null) ? " via " + appName : "");
 }
 
 void main(List<String> arguments) {
   
-  double p, l;
-
-  print('Masukkan panjang: ');
-  p = double.tryParse(stdin.readLineSync());
-  print('Masukkan lebar: ');
-  l = double.tryParse(stdin.readLineSync());
-
-  print('hasil = ' + luas_segiempat(p, l).toString());
+  print(say("Ikbal", "Hello")); // tidak diberi argument ke named parameter
+  print(say("Ikbal", "Hello", to: "Bale", appName: "Whatsapp")); // jika diberi argument ke named parameter
 
 }
