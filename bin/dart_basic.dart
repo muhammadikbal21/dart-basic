91ae6083a2ad03782a7a36c17399152ee818022e
muhammadikbal21 <muhammadikbal21@gmail.com>
2021-08-03 09:42:19 +0700

    mengakses list array menggunakan indeks

--- bin/dart_basic.dart
+++ bin/dart_basic.dart
@@ -1,11 +1,10 @@
 import 'dart:io';
 
-int doMathOperator(int number1, int number2, Function(int, int) operator) {
-  return operator(number1, number2);
-}
-
 void main(List<String> arguments) {
   
-  print(doMathOperator(1, 2, (a, b) => a * b));
+  List<int> list = [1, 2, 3];
+  int e = list[0];
+
+  print(e);
 
 }
