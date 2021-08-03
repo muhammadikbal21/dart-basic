a4fcd372ec348bac986709267401fb10bd2a9c5a
muhammadikbal21 <muhammadikbal21@gmail.com>
2021-08-03 07:44:37 +0700

    menggunakan default parameter

--- bin/dart_basic.dart
+++ bin/dart_basic.dart
@@ -7,20 +7,39 @@ import 'dart:io';
 
 // ini contoh penggunaan named parameter
 String say(String from, String message, {String to, String appName}) {
-  return from + " say " + message + ((to != null) ? " to " + to : "") + ((appName != null) ? " via " + appName : "");
+  return from +
+      " say " +
+      message +
+      ((to != null) ? " to " + to : "") +
+      ((appName != null) ? " via " + appName : "");
 }
 
 // ini contoh penggunaan position parameter
 String say2(String from, String message, [String to, String appName]) {
-  return from + " say " + message + ((to != null) ? " to " + to : "") + ((appName != null) ? " via " + appName : "");
+  return from +
+      " say " +
+      message +
+      ((to != null) ? " to " + to : "") +
+      ((appName != null) ? " via " + appName : "");
+}
+
+// ini contoh penggunaan default parameter
+// default parameter dapat digunakan pada named parameter dan position parameter
+String say3(String from, String message, [String to, String appName = "Facebook"]) {
+  return from +
+      " say " +
+      message +
+      ((to != null) ? " to " + to : "") +
+      ((appName != null) ? " via " + appName : "");
 }
 
 void main(List<String> arguments) {
-  
   // kelebihan named parameter adalah posisi argument tidak harus sesuai dengan parameter di function asalkan disertakan nama parameternya agar tidak rancu / membingungkan
   print('named parameter: ' + say("Ikbal", "Hello", appName: "Whatsapp"));
 
   // position parameter harus menetapkan argumentnya sesuai dengan parameter di function
   print('position parameter: ' + say2("Ikbal", "Hello", "Bale", "Whatsapp"));
 
+  // disini kita tidak memberi argument terakhir
+  print('default parameter: ' + say3("Ikbal", "Hello", "Bale"));
 }
