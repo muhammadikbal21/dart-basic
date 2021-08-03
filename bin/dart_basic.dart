ff444ff676a0e482193c422dcb9da39066c353fc
muhammadikbal21 <muhammadikbal21@gmail.com>
2021-08-03 10:01:15 +0700

    penggunaan insert untuk menyisipkan elemen kedalam List menggunakan indeks

--- bin/dart_basic.dart
+++ bin/dart_basic.dart
@@ -6,6 +6,7 @@ void main(List<String> arguments) {
   List<int> list = [1, 2, 3];
 
   myList.addAll(list);
+  myList.insert(0, 100); // insert untuk menyisipkan elemen kedalam List menggunakan indeks
 
   myList.forEach((bilangan) {
     print(bilangan);
