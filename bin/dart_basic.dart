37cd93dd6f32d77f712ab1ff6f090f7f41535223
muhammadikbal21 <muhammadikbal21@gmail.com>
2021-08-03 11:26:40 +0700

    penggunaan clear untuk menghapus elemen di List

--- bin/dart_basic.dart
+++ bin/dart_basic.dart
@@ -5,12 +5,9 @@ void main(List<String> arguments) {
   List<int> myList = [];
   List<int> list = [0, 1, 2, 3, 20, 30, 40, 50];
 
-  // sublist adalah mengambil sebagian dari list lain menggunakan jarak indeks awal dan indeks akhir
-  // sublist bersifat eksklusif, artinya jarak indeks terakhir elemennya tidak termasuk
-  // disini indeks terakhir tidak diberikan, maka elemen akan diambil dari yang diinginkan sampai elemen terakhir
-  myList = list.sublist(4);
+  list.clear(); // menghapus semua elemen di List
 
-  myList.forEach((bilangan) {
+  list.forEach((bilangan) {
     print(bilangan);
   });
 
