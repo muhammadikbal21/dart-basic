77bd25fdbb1df552b183bcc6853c1b8fef78403f
muhammadikbal21 <muhammadikbal21@gmail.com>
2021-08-03 11:08:28 +0700

    penggunaan removeWhere dengan kondisi untuk mendapatkan nilai ganjil

--- bin/dart_basic.dart
+++ bin/dart_basic.dart
@@ -6,9 +6,9 @@ void main(List<String> arguments) {
   List<int> list = [0, 1, 2, 3, 20, 30, 40, 50];
 
   myList.addAll(list);
-  // removeRange bersifat eksklusif, artinya jarak indeks terakhir elemennya tidak termasuk
-  // disini 7 adalah jarak indeks terakhir, maka elemennya tidak ikut terhapus
-  myList.removeRange(4, 7); // menghapus dengan jarak index yang diinginkan
+  // removeWhere adalah menghapus dengan kondisi, jika kondisi terpenuhi maka elemen akan dihapus
+  // disini kita menggunakan logic untuk mendapatkan nilai ganjil, dan akan dihapus jika elemen tersebut bernilai ganjil
+  myList.removeWhere((number) => number % 2 != 0);
 
   myList.forEach((bilangan) {
     print(bilangan);
