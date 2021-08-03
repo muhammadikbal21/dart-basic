61ff7b1272cd59859253bc31472672f40dac859e
muhammadikbal21 <muhammadikbal21@gmail.com>
2021-08-03 09:58:05 +0700

    penggunaan addAll untuk menambah banyak elemen di List

--- bin/dart_basic.dart
+++ bin/dart_basic.dart
@@ -3,8 +3,9 @@ import 'dart:io';
 void main(List<String> arguments) {
   
   List<int> myList = [];
+  List<int> list = [1, 2, 3];
 
-  myList.add(10);
+  myList.addAll(list);
 
   myList.forEach((bilangan) {
     print(bilangan);
