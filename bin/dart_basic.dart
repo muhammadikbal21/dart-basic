245686f392e636cf4dfdaef3b3b60c181941d5a8
muhammadikbal21 <muhammadikbal21@gmail.com>
2021-08-03 11:47:14 +0700

    penggunaan every untuk mengecek isi List dengan menggunakan kondisi

--- bin/dart_basic.dart
+++ bin/dart_basic.dart
@@ -5,27 +5,16 @@ void main(List<String> arguments) {
   List<int> myList = [];
   List<int> list = [21, 11, 2, 0, 45, 30, 40, 10];
 
-  list.sort(); // mengurutkan elemen di List secara menaik
+  list.removeWhere((element) => element % 2 == 0);
 
-  print('urutkan secara menaik');
+  if (list.every((number) => number % 2 != 0)) {
+    print('semua elemen ganjil');
+  } else {
+    print('tidak semua elemen ganjil');
+  }
+
   list.forEach((bilangan) {
     print(bilangan);
   });
-
-  list.sort((a, b) => b - a);
-
-  print('urutkan secara menurun');
-  list.forEach((bilangan) {
-    print(bilangan);
-  });
-
-  // sort logic:
-  // 1. membandingkan 2 bilangan, contoh a dan b
-  // 2. jika hasil perbandingan a dan b negatif maka a urutan paling depan
-  // kalau positif, maka b urutan paling depan.
-  // contoh:
-  // a = 4
-  // b = 5
-  // b - a = 1 positif, maka a ditaruh di kanan
 
 }
