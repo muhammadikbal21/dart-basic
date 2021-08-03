130cba8013afba34ee9ded733de1c85f45089702
muhammadikbal21 <muhammadikbal21@gmail.com>
2021-08-03 10:16:14 +0700

    penggunaan removeLast di List

--- bin/dart_basic.dart
+++ bin/dart_basic.dart
@@ -8,8 +8,7 @@ void main(List<String> arguments) {
   myList.addAll(list);
   // iterable adalah kumpulan elemen elemen atau disebut dengan List
   myList.insertAll(2, [20, 30]); // insertAll untuk menyisipkan elemen kedalam List menggunakan indeks dan iterable
-  // jika menggunakan remove dan valuenya terdapat duplikasi (sama) maka yang dihapus adalah value di index yang diawal
-  myList.remove(3); // menghapus elemen List yang mempunyai value 3
+  myList.removeLast(); // menghapus elemen terakhir di List
 
   myList.forEach((bilangan) {
     print(bilangan);
