aef43f886f10363a5117a4bfcc4bd0e189924c99
muhammadikbal21 <muhammadikbal21@gmail.com>
2021-08-03 08:13:38 +0700

    menyimpan function kedalam variable

--- bin/dart_basic.dart
+++ bin/dart_basic.dart
@@ -3,8 +3,15 @@ import 'dart:io';
 // arrow expression => menyingkat function menjadi 1 line, ini hanya berlaku jika function memiliki nilai kembali (return) 
 double luas_segiempat(double panjang, double lebar) => panjang * lebar;
 
+// function dapat disimpan dalam sebuah variable
+// function dapat dimasukkan sebagai parameter
+// function dapat dijadikan nilai kembalian
+
 void main(List<String> arguments) {
   
-  print(luas_segiempat(6, 7));
+  Function f;
+  f = luas_segiempat; // function disimpan ke variable
+
+  print(f(6.0, 7.0));
 
 }
