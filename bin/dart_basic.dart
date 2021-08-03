bd2b0b501137f3e048fb6abd1fd2013c7273fd66
muhammadikbal21 <muhammadikbal21@gmail.com>
2021-08-03 11:50:15 +0700

    penggunaan isEmpty dan isNotEmpty untuk mengecek isi elemen di List

--- bin/dart_basic.dart
+++ bin/dart_basic.dart
@@ -5,12 +5,12 @@ void main(List<String> arguments) {
   List<int> myList = [];
   List<int> list = [21, 11, 2, 0, 45, 30, 40, 10];
 
-  list.removeWhere((element) => element % 2 == 0);
-
-  if (list.every((number) => number % 2 != 0)) {
-    print('semua elemen ganjil');
-  } else {
-    print('tidak semua elemen ganjil');
+  if (list.isEmpty) {
+    print('kosong');
+  
+  }
+  if (list.isNotEmpty) {
+    print('tidak kosong');
   }
 
   list.forEach((bilangan) {
