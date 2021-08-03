8b5955dd8004fe452fabd3c8e244684ab05d2dd7
muhammadikbal21 <muhammadikbal21@gmail.com>
2021-08-03 11:54:16 +0700

    penggunaan Set untuk menyimpan elemen yang unik

--- bin/dart_basic.dart
+++ bin/dart_basic.dart
@@ -3,17 +3,13 @@ import 'dart:io';
 void main(List<String> arguments) {
   
   List<int> myList = [];
-  List<int> list = [21, 11, 2, 0, 45, 30, 40, 10];
+  List<int> list = [21, 11, 2, 0, 11, 45, 30, 0, 40, 10];
 
-  if (list.isEmpty) {
-    print('kosong');
-  
-  }
-  if (list.isNotEmpty) {
-    print('tidak kosong');
-  }
+  // set adalah kumpulan data yang berisi elemen yang unik (tidak ada yang sama)
+  Set<int> setList;
+  setList = list.toSet();
 
-  list.forEach((bilangan) {
+  setList.forEach((bilangan) {
     print(bilangan);
   });
 
