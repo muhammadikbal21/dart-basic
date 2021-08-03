f363290ad6dbb4633ab66e501a18f5ea2b5ce2c3
muhammadikbal21 <muhammadikbal21@gmail.com>
2021-08-03 11:37:30 +0700

    penggunaan sort untuk mengurutkan elemen di List secara ascending dan descending

--- bin/dart_basic.dart
+++ bin/dart_basic.dart
@@ -3,12 +3,29 @@ import 'dart:io';
 void main(List<String> arguments) {
   
   List<int> myList = [];
-  List<int> list = [0, 1, 2, 3, 20, 30, 40, 50];
+  List<int> list = [21, 11, 2, 0, 45, 30, 40, 10];
 
-  list.clear(); // menghapus semua elemen di List
+  list.sort(); // mengurutkan elemen di List secara menaik
 
+  print('urutkan secara menaik');
   list.forEach((bilangan) {
     print(bilangan);
   });
+
+  list.sort((a, b) => b - a);
+
+  print('urutkan secara menurun');
+  list.forEach((bilangan) {
+    print(bilangan);
+  });
+
+  // sort logic:
+  // 1. membandingkan 2 bilangan, contoh a dan b
+  // 2. jika hasil perbandingan a dan b negatif maka a urutan paling depan
+  // kalau positif, maka b urutan paling depan.
+  // contoh:
+  // a = 4
+  // b = 5
+  // b - a = 1 positif, maka a ditaruh di kanan
 
 }
