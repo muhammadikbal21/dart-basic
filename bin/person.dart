72174de63740e8025b84937bbd359b4857daa47c
muhammadikbal21 <muhammadikbal21@gmail.com>
2021-08-08 00:23:11 +0700

    penggunaan super pada class child agar dapat menggunakan parameter dari constructor parentnya

--- bin/person.dart
+++ bin/person.dart
@@ -4,7 +4,7 @@ class Person {
   // ini adalah constructor
   // constructor = method khusus yang dipanggil pertama kali ketika kita membuat object dari sebuah kelas, dan nama constructor sama dengan nama kelasnya
   // consturcotr tidak memiliki tipe kembalian (return)
-  Person({String name = 'no name'}) {  // constructor dengan optional named parameter
+  Person(String name) {  // constructor dengan optional named parameter
     print('constructor person');
     this.name = name;
   }
--- bin/student.dart
+++ bin/student.dart
@@ -1,7 +1,8 @@
 import 'person.dart';
 
 class Student extends Person {
-  Student() {
+  Student() : super('student baru') { // karena Student child dari Person, maka kita harus menggunakan super agar dapat mengambil isi constructor dari parentnya
+    // parameter 'student baru' akan dikirim ke constructor parent dan valuenya akan digunakan pada objectnya
     print('constructor student');
   }
 }
