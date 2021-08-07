af31da7233c430d98d73e8fe892501d61e155b07
muhammadikbal21 <muhammadikbal21@gmail.com>
2021-08-07 23:18:42 +0700

    constructor dengan optional named parameter

--- bin/dart_basic.dart
+++ bin/dart_basic.dart
@@ -4,8 +4,10 @@ import 'person.dart';
 
 void main(List<String> arguments) {
 
-  Person person = Person('Joko'); // Person() adalah constructor yang berfungsi membuat object dari kelasnya
+  Person person = Person(); // Person() adalah constructor yang berfungsi membuat object dari kelasnya
+  Person person2 = Person(name: 'Joko');
 
   print('nama saya = ' + person.name);
+  print('nama saya = ' + person2.name);
 
 }
--- bin/person.dart
+++ bin/person.dart
@@ -4,7 +4,7 @@ class Person {
   // ini adalah constructor
   // constructor = method khusus yang dipanggil pertama kali ketika kita membuat object dari sebuah kelas, dan nama constructor sama dengan nama kelasnya
   // consturcotr tidak memiliki tipe kembalian (return)
-  Person(String name) { 
+  Person({String name = 'no name'}) {  // constructor dengan optional named parameter
     this.name = name;
   }
 }
