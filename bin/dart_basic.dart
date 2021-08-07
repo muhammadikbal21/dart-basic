835361ba3b7a8094bb72b486044aef88a428271e
muhammadikbal21 <muhammadikbal21@gmail.com>
2021-08-08 01:18:07 +0700

    penggunaan optional named parameter pada class Student yang mengoverride constructor parentnya (super)

--- bin/dart_basic.dart
+++ bin/dart_basic.dart
@@ -5,8 +5,10 @@ import 'student.dart';
 
 void main(List<String> arguments) {
 
-  Person person = Student(); // object person dengan menggunakan constructor Student() dimana Student mengextends Person yang memiliki constructor default named parameter
+  Person person = Student(studentName: 'Joni'); // object person dengan menggunakan constructor Student() dimana Student mengextends Person yang memiliki constructor default named parameter
+  Person person2 = Student();
 
-  print('nama saya = ' + person.name);
+  print('nama saya = ' + person.name); // object ini menggunakan parameter 'Joni
+  print('nama saya = ' + person2.name); // object ini menggunakan default parameter yang ada di class Student
 
 }
--- bin/student.dart
+++ bin/student.dart
@@ -1,8 +1,9 @@
 import 'person.dart';
 
 class Student extends Person {
-  Student() : super('student baru') { // karena Student child dari Person, maka kita harus menggunakan super agar dapat mengambil isi constructor dari parentnya
-    // parameter 'student baru' akan dikirim ke constructor parent dan valuenya akan digunakan pada objectnya
+  Student({String studentName: 'student baru'}) : super(name: studentName) { // karena Student child dari Person, maka kita harus menggunakan super agar dapat mengambil isi constructor dari parentnya
+    // parameter 'student baru' akan menjadi default parameter jika object tidak memiliki parameter
+    // jika object memiliki parameter akan menggunakan named parameter dari super
     print('constructor student');
   }
 }
