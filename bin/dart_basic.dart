73c1ab57ec4ff1a81dc91cb9b31e37899dcf7087
muhammadikbal21 <muhammadikbal21@gmail.com>
2021-08-14 22:45:45 +0700

    membuat custom sort untuk mengurutkan role nya secara custom

--- bin/dart_basic.dart
+++ bin/dart_basic.dart
@@ -15,9 +15,9 @@ void main(List<String> arguments) {
   // jika nilai kembaliannya negatif, maka object pertama akan diletakkan disebelah kiri dari object kedua
   // jika nilai kembaliannya positif, maka object pertama akan diletakkan disebelah kanan dari object kedua
   person.sort((p1, p2) {
-    // menggunakan kondisi untuk mengurutkan secara Abjad, lalu diurutkan lagi menurut angka secara menaik
-    if (p1.role.compareTo(p2.role) != 0) { // tidak sama dengan 0 berarti bisa negatif bisa postif
-      return p1.role.compareTo(p2.role); // ini mengurutkan huruf secara abjad (A-Z)
+    // menggunakan kondisi dengan method get dari class object
+    if (p1.roleWeight - p2.roleWeight != 0) { 
+      return p1.roleWeight - p2.roleWeight; // mengurutkan dengan cara custom
     } else {
       return p1.age.compareTo(p2.age); // ini mengurutkan angka secara menaik
     }
@@ -33,4 +33,18 @@ class CustomSortAndForEach {
   final int age;
 
   CustomSortAndForEach(this.role, this.age);
+
+  // method get untuk mengcustom urutan role nya
+  int get roleWeight {
+    switch (role) {
+      case 'Merchant':
+        return 1;
+        break;
+      case 'Administrator':
+        return 2;
+        break;
+      default:
+        return 3;
+    }
+  }
 }
