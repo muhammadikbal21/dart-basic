15d4a1eaaaba7cf0024093815cb8d5968a400f1c
muhammadikbal21 <muhammadikbal21@gmail.com>
2021-08-14 18:35:59 +0700

    penggunaan cascade notation untuk memanggil method dengan satu object yang sama secara berurutan

--- bin/dart_basic.dart
+++ bin/dart_basic.dart
@@ -1,9 +1,9 @@
 import 'dart:io';
 
 void main(List<String> arguments) async {
-  MonsterInc monsterInc = MonsterInc(status: MonsterIncStatus.POISONED);
-
-  monsterInc.move();
+  MonsterInc monsterInc = MonsterInc(status: MonsterIncStatus.POISONED)
+  ..move() // ini adalah cascade notation
+  ..eat(); // cascade notation berfungsi untuk memanggil beberapa method terhadap satu buah object yang sama secara berurutan
 }
 
 enum MonsterIncStatus {
@@ -30,5 +30,9 @@ class MonsterInc {
         break;
       default:
     }
+  }
+
+  void eat() {
+    print('MonsterInc is eating Nasi Padang');
   }
 }
