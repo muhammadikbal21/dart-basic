8e154932fb77d2e7ab6ba8ed988d2d9b6d63b94e
muhammadikbal21 <muhammadikbal21@gmail.com>
2021-08-14 11:53:12 +0700

    penggunaan future pada method async dan await untuk mendapatkan jeda, tetapi tidak menunggu hasilnya ketika di panggil methodnya

--- bin/dart_basic.dart
+++ bin/dart_basic.dart
@@ -6,13 +6,13 @@ import 'package:dart_basic/static_keyword.dart';
 import 'person.dart';
 import 'student.dart';
 
-void main(List<String> arguments) {
+void main(List<String> arguments) async {
   var p = AsyncAwaitFuture();
 
   // ini adalah contoh synchronus: melakukan perintah secara urutan
   print('job 1');
   print('job 2');
-  p.getData();
+  p.getDataAsync(); // jika tidak menggunakan await maka method asynchronus akan akan lanjut ke perintah selanjutnya sampai hasil tunggunya keluar
   print('job 3: ' + p.name);
   print('job 4');
   print('job 5');
@@ -25,6 +25,16 @@ class AsyncAwaitFuture {
 
   void getData() { // Misalnya ambil data dari database setelah 3 detik
     name = 'Muhammad Ikbal'; 
+    print('get data [done]');
+  }
+
+  // Futuer adalah tipe kembalian dari asynchronus tetapi ia tidak langsung return, bisa nunggu beberapa detik baru dapat return nya
+  // karena disini tidak ada return, maka kita menggunakan void
+  Future<void> getDataAsync() async {
+    // await adalah method untuk mendapatkan hasil yang telah ditunggu
+    await Future.delayed(Duration(seconds: 3));
+
+    name = 'Bale';
     print('get data [done]');
   }
 }
