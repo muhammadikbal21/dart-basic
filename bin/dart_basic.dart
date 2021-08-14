172e8dcde0168991444d5898826f53a82f394a1c
muhammadikbal21 <muhammadikbal21@gmail.com>
2021-08-14 19:43:17 +0700

    penggunaan required yaitu valuenya wajib diisi

--- bin/dart_basic.dart
+++ bin/dart_basic.dart
@@ -1,40 +1,14 @@
-import 'dart:io';
+import 'package:meta/meta.dart';
 
 void main(List<String> arguments) async {
-  MonsterInc monsterInc = MonsterInc(status: MonsterIncStatus.POISONED);
-  print('Ini Contoh Pemutus Object diatas'); // jika terjadi pemutusan seperti ini, kita dapat mengatasinya seperti dibawah ini
-  monsterInc
-  ..move() // ini adalah cascade notation
-  ..eat(); // cascade notation berfungsi untuk memanggil beberapa method terhadap satu buah object yang sama secara berurutan
+  RequiredAssertTryCatch p;
+  p = RequiredAssertTryCatch(name: null);
+  print(p.age);
 }
 
-enum MonsterIncStatus {
-  NORMAL,
-  POISONED,
-  CONFUSED
-}
+class RequiredAssertTryCatch {
+  final String name;
+  final int age;
 
-class MonsterInc {
-  final MonsterIncStatus status;
-
-  MonsterInc({this.status = MonsterIncStatus.NORMAL});
-
-  void move() {
-    switch (status) {
-      case MonsterIncStatus.NORMAL:
-        print('Monster Inc is moving');
-        break;
-      case MonsterIncStatus.POISONED:
-        print('Monster Inc cannot move. It is dying. It needs help');
-        break;
-      case MonsterIncStatus.CONFUSED:
-        print('Monster Inc is spinning. Dart language is confusing');
-        break;
-      default:
-    }
-  }
-
-  void eat() {
-    print('MonsterInc is eating Nasi Padang');
-  }
+  RequiredAssertTryCatch({@required this.name, this.age = 0}); // required valuenya wajib diisi
 }
