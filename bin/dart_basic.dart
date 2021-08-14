681a36cf52837554dbf4521ac8dd740bdf756384
muhammadikbal21 <muhammadikbal21@gmail.com>
2021-08-14 22:40:33 +0700

    penggunaan sort dengan kondisi untuk mengurutkan secara Abjad, lalu diurutkan lagi menurut angka secara menaik

--- bin/dart_basic.dart
+++ bin/dart_basic.dart
@@ -14,7 +14,14 @@ void main(List<String> arguments) {
   // sort memiliki function compare untuk membandingkan element dengan tipe kembalian integer
   // jika nilai kembaliannya negatif, maka object pertama akan diletakkan disebelah kiri dari object kedua
   // jika nilai kembaliannya positif, maka object pertama akan diletakkan disebelah kanan dari object kedua
-  person.sort((p1, p2) => p1.role.compareTo(p2.role)); // disini compare juga memiliki tipe kembalian String, menggunakan compareTo yang akan mengurutkan string berdasarkan abjad secara menaik (A-Z)
+  person.sort((p1, p2) {
+    // menggunakan kondisi untuk mengurutkan secara Abjad, lalu diurutkan lagi menurut angka secara menaik
+    if (p1.role.compareTo(p2.role) != 0) { // tidak sama dengan 0 berarti bisa negatif bisa postif
+      return p1.role.compareTo(p2.role); // ini mengurutkan huruf secara abjad (A-Z)
+    } else {
+      return p1.age.compareTo(p2.age); // ini mengurutkan angka secara menaik
+    }
+  }); 
 
   person.forEach((element) {
     print(element.role + ' - ' + element.age.toString());
