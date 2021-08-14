c811b930b142379ac588fc31332e3bee30765c78
muhammadikbal21 <muhammadikbal21@gmail.com>
2021-08-14 19:54:09 +0700

    penggunaan assert untuk menegaskan kalau parameter harus ada valuenya

--- bin/dart_basic.dart
+++ bin/dart_basic.dart
@@ -2,7 +2,7 @@ import 'package:meta/meta.dart';
 
 void main(List<String> arguments) async {
   RequiredAssertTryCatch p;
-  p = RequiredAssertTryCatch(name: null);
+  p = RequiredAssertTryCatch(name: null); // ini harus diisi, karena constructor menggunakan assert, jika tidak maka akan muncul error
   print(p.age);
 }
 
@@ -10,5 +10,8 @@ class RequiredAssertTryCatch {
   final String name;
   final int age;
 
-  RequiredAssertTryCatch({@required this.name, this.age = 0}); // required valuenya wajib diisi
+  RequiredAssertTryCatch({@required this.name, this.age = 0}) { // required valuenya wajib diisi
+    // assert menegaskan kalau parameter harus ada valuenya, jika di debug (F5) maka akan kelihatan errornya dan program dipaksa berhenti
+    assert(name != null, "Kamu harus memberi nama"); 
+  }
 }
