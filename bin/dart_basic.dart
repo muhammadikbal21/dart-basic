7176d88d66165cfb06f05f7ee25356ff790da867
muhammadikbal21 <muhammadikbal21@gmail.com>
2021-08-14 15:54:23 +0700

    penggunaan final variable yang bisa digunakan

--- bin/dart_basic.dart
+++ bin/dart_basic.dart
@@ -7,36 +7,15 @@ import 'person.dart';
 import 'student.dart';
 
 void main(List<String> arguments) async {
-  var p = AsyncAwaitFuture();
-
-  // ini adalah contoh synchronus: melakukan perintah secara urutan
-  print('job 1');
-  print('job 2');
-  // menggunakan then dimana ia menerima function (anonymous) yang akan dieksekusi, ia juga menerima parameter
-  p.getDataAsync().then((_) => { // karena method getDataAsync tidak mempunyai return maka kita akan mengabaikan parameternya menggunakan underscore (_)
-    print('job 3: ' + p.name) // ini akan dieksekusi ketika hasil delay dari function sudah selesai
-  });
-  print('job 4');
-  print('job 5');
-  print('job 6');
-  print('job 7');
+  // final variable harus diinisialisasi, tidak boleh hanya dengan titik koma (;)
+  // nilainya juga tidak dapat diubah (diupdate)
+  // dapat diisi didalam parameter constructor
+  // dapat dideklarasikan dimanapun
+  final person; 
 }
 
-class AsyncAwaitFuture {
-  String name = 'default name';
+class FinalClass {
+  final int number; // jika final variable nya tidak diinisialisasi maka dapat melakukan cara seperti ini
 
-  void getData() { // Misalnya ambil data dari database setelah 3 detik
-    name = 'Muhammad Ikbal'; 
-    print('get data [done]');
-  }
-
-  // Futuer adalah tipe kembalian dari asynchronus tetapi ia tidak langsung return, bisa nunggu beberapa detik baru dapat return nya
-  // karena disini tidak ada return, maka kita menggunakan void
-  Future<void> getDataAsync() async {
-    // await adalah method untuk mendapatkan hasil yang telah ditunggu
-    await Future.delayed(Duration(seconds: 3));
-
-    name = 'Bale';
-    print('get data [done]');
-  }
+  FinalClass({this.number});
 }
