5345ed5b2a37712a9bee4e7f998b684ca0304436
muhammadikbal21 <muhammadikbal21@gmail.com>
2021-08-14 16:05:06 +0700

    penggunaan const variable dan mengecek object yang sama menggunakan method identical

--- bin/dart_basic.dart
+++ bin/dart_basic.dart
@@ -7,15 +7,37 @@ import 'person.dart';
 import 'student.dart';
 
 void main(List<String> arguments) async {
-  // final variable harus diinisialisasi, tidak boleh hanya dengan titik koma (;)
-  // nilainya juga tidak dapat diubah (diupdate)
-  // dapat diisi didalam parameter constructor
-  // dapat dideklarasikan dimanapun
-  final person; 
+  // const variable dideklarasikan hanya sebagai static atau local variable (didalam method), dan di top level (diluar atau paling luar method)
+  // keuntungan const:
+  // 1. kita tidak membuat object berulang ulang, kalo bentuknya yang sama
+  // 2. lebih irit memori
+  // 3. memiliki performa yang baik
+
+  // tanpa menggunakan const, kedua object ini tidak identik
+  var a = ConstClass(number: 5);
+  var b = ConstClass(number: 5);
+  print('Identik atau Sama Persis kah? : '  + identical(a, b).toString()); // identical adalah method untuk mengecek apakah objectnya sama
+
+  // menggunakan const, kedua object ini identik
+  var c = const ConstClass(number: 5);
+  var d = const ConstClass(number: 5);
+  print('Identik atau Sama Persis kah? : '  + identical(c, d).toString());
+  
+  // salah satu menggunakan const, kedua object ini tidak identik
+  var e = const ConstClass(number: 5);
+  var f = ConstClass(number: 5);
+  print('Identik atau Sama Persis kah? : '  + identical(e, f).toString());
+
 }
 
 class FinalClass {
   final int number; // jika final variable nya tidak diinisialisasi maka dapat melakukan cara seperti ini
 
   FinalClass({this.number});
+}
+
+class ConstClass {
+  final int number;
+
+  const ConstClass({this.number});
 }
