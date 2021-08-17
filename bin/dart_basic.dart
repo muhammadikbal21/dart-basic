fa6b1c8b817b5ec9af9f2cddade5498fa8803819
muhammadikbal21 <muhammadikbal21@gmail.com>
2021-08-17 13:24:28 +0700

    contoh tanpa menerapkan prinsip singleton

--- bin/dart_basic.dart
+++ bin/dart_basic.dart
@@ -1,44 +1,13 @@
+import 'dart:developer';
+
 void main(List<String> arguments) {
+  Service service1 = Service();
+  Service service2 = Service();
+
+  if (service1 == service2) {
+    print('sama');
+  } else {
+    print('beda');
+  }
   
-  Hero a = Hero(10);
-  Hero b = Hero(9);
-
-  print(a > b); 
-}
-
-class Hero {
-  final int power;
-
-  Hero(this.power);
-
-  Hero operator + (var other) { // membuat function agar dapat melakukan penjumlahan pada object dan dapat menjumlahkan dengan integer lainnya yang bukan object
-    if (other is Hero) {
-      return Hero(power + other.power);
-    } else if (other is int) {
-      return Hero(power + other);
-    } else {
-      return this;
-    }
-  }
-
-  @override
-  bool operator == (var other) { // membuat function ketika nilai parameternya sama maka akan mereturn true
-    if (other is Hero) {
-      if (other.power == power) {
-        return true;
-      }
-    }
-
-    return false;
-  }
-
-  bool operator > (var other) { // membuat operator perbandingan lebih besar (>) untuk membandingkan parameter objectnya
-    if (other is Hero) {
-      if (power > other.power) {
-        return true;
-      }
-    }
-
-    return false;
-  }
 }
---
+++ bin/services.dart
@@ -0,0 +1,8 @@
+import 'package:dart_basic/solid/D/d_right.dart';
+
+class Services {
+  Future<User> getUserData() async {
+    // kita asumsikan ini function mendapatkan data lalu kita mereturnnya
+    return User();
+  }
+}
