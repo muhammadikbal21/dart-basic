2f8c2c9021360447a22298d9194ff5193a5a88c5
muhammadikbal21 <muhammadikbal21@gmail.com>
2021-08-18 20:16:05 +0700

    pengertian var dan macam macam penggunaan var

--- bin/dart_basic.dart
+++ bin/dart_basic.dart
@@ -1,16 +1,17 @@
 void main(List<String> arguments) {
-  // tipe dynamic: dapat dipasang pada tipe object apapun
-  // kekurangannya dia tidak bisa mendeteksi ada method apa aja yang ada didalam method tersebut (suggest nya)
-  dynamic myDynamic;
+  // tipe var: untuk memungkinan variable ini untuk menunjuk object tipe apapun tapi hanya sekali diawal aja, 
+  // setelah itu object yang dimasukkan kedalam object tadi harus setipe dengan object yang pertama kali kita masukkan
+  var myVar = 12; // pada deklarasi var diawal kita sudah menetapkan valuenya menjadi integer
+  myVar = 'Hello'; // ini tidak dapat mengubah valuenya menjadi string karena pada deklarasi diawal sudah menggunakan integer sebagai valuenya
+  print(myVar);
 
-  myDynamic = 'Hello';
-  myDynamic = 12;
-  myDynamic = Person();
+  var myVar2; // jika deklarasi diawal tidak menetapkan valuenya, maka secara default tipe var bersifat dynamic
+  myVar2 = 12;
+  myVar2 = 'Hello World';
+  print(myVar2);
 
-  if (myDynamic is Person) { // kita juga dapat menggunakan 'is' pada kondisi if untuk mendapatkan suggest dari object tersebut
-    print(myDynamic.name);
-  }
-  
+  var myVar3 = Person();
+  print(myVar3.name);
 }
 
 class Person {
